744f02d4c2cd27aa51891e62fab31b39816e983a
spencer barriball <sbarriball@me.com>
2025-03-07 04:41:08 +0000

    Update vite config

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000'),
-  },
 })
+
 
