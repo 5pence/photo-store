2ab2e2bbb0466a11364bbc88a151f061a4f0b0bd
spencer barriball <sbarriball@me.com>
2025-03-07 04:38:16 +0000

    Add URL for api

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(import.meta.env.VITE_BACKEND_URL)
-  }
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000'),
+  },
 })
+
