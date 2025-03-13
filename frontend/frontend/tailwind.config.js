cefcb36b2229443a66d5a931692ad21853b63b01
spencer barriball <sbarriball@me.com>
2025-03-13 17:12:56 +0000

    Remove darkmode

--- frontend/frontend/tailwind.config.js
+++ frontend/frontend/tailwind.config.js
@@ -13,19 +13,9 @@ export default {
           "0%": { opacity: 0, transform: "translateY(10px)" },
           "100%": { opacity: 1, transform: "translateY(0)" },
         },
-        fadeOut: {
-          "0%": { opacity: 1, transform: "translateY(0)" },
-          "100%": { opacity: 0, transform: "translateY(10px)" },
-        },
-        darkModeFadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
       },
       animation: {
         "fade-in": "fadeIn 1.1s ease-in-out",
-        "fade-out": "fadeOut 1s ease-in-out",
-        "dark-mode-fade": "darkModeFadeIn 0.5s ease-in-out",
       },
       colors: {
         "black-bean": "#220B07",
@@ -33,7 +23,7 @@ export default {
         "muted-blue": "#7E7F9A", // Muted Blue
         "dark": "#1C1C1C", // Deep Charcoal
         "light-gray": "#E5E5E5", // Soft Gray
-        "warm-white": "#F8F6F2", // Warm Off-White
+        "warm-white": "#F8F6F2" // Warm Off-White
       },
       fontFamily: {
         proxima: ['proxima-nova', 'sans-serif'],
@@ -46,7 +36,6 @@ export default {
   plugins: [daisyui],
   daisyui: {
     themes: [
-        "dark",
       {
         spencerTheme: {
           "primary": "#D64933", // Rusty Red
@@ -56,21 +45,8 @@ export default {
           "base-100": "#F8F6F2", // Warm Off-White
           "info": "#7E7F9A", // Muted Blue
           "success": "#95C623", // Olive Green
-          "warning": "#D64933", // Rusty Red
+          "warning": "#D64933", // Rusty Red (as warning for contrast)
           "error": "#9C0D38", // Deep Red
-
-          /* Dark Mode */
-          "[data-theme='dark'] &": {
-            "primary": "#F8F6F2",
-            "secondary": "#7E7F9A",
-            "accent": "#1C1C1C",
-            "neutral": "#E5E5E5",
-            "base-100": "#220B07", // Dark background
-            "info": "#7E7F9A",
-            "success": "#95C623",
-            "warning": "#D64933",
-            "error": "#9C0D38",
-          },
         },
       },
     ],
